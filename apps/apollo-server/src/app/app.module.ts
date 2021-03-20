be420c981c2f28d16de8ee2d1a617ba44027c7b1
HowToBeAHappyBoy <seozi@101.inc>
2021-03-20 16:54:15 +0900

    feat(apollo-server): graphql 구조 설정

--- apps/apollo-server/src/app/app.module.ts
+++ apps/apollo-server/src/app/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+
+import { MONGO_URL } from '../constants';
+import { StoryModule } from './story/story.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(MONGO_URL),
+    StoryModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'apps/apollo-server/src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      debug: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
