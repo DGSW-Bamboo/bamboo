526a8d3b5f7e7b35dd836120b294bd3c4f2d8f5f
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-03-25 14:10:01 +0900

    core(web) Primer 및 스타일링 환경 세팅

--- apps/web/src/style/GlobalStyle.tsx
+++ apps/web/src/style/GlobalStyle.tsx
@@ -1,18 +1,11 @@
-/** @jsx jsx */
-import { Global, css, jsx } from '@emotion/react';
+import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = () => {
-  return (
-    <Global
-      styles={css`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
-      `}
-    />
-  );
-};
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 export default GlobalStyle;
