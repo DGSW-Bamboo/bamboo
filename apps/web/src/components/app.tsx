40f4f93604a826245f25b4da5aa5990e23b74e52
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-03-25 10:32:39 +0900

    add(apollo-client): setup project

---
+++ apps/web/src/components/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+export const App = () => {
+  return <div>Hello</div>;
+};
+
+export default App;
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from 'components/app';
+import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
-
-import App from './app/app';
+import { client } from 'client';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RecoilRoot>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
