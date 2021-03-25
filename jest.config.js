497bb504bde644db5ff89c25fc23fff603be3746
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-03-25 09:57:17 +0900

    init base project files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/apollo-server', '<rootDir>/libs/auth'],
+  projects: [
+    '<rootDir>/apps/apollo-server',
+    '<rootDir>/libs/auth',
+    '<rootDir>/apps/web',
+  ],
 };
