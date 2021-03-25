ea35b49abf03fbeae708e3eff1fe751f5a43f50c
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-03-25 13:42:45 +0900

    fix(web) 누락된 스타일 수정

--- apps/web/src/style/GlobalStyle.tsx
+++ apps/web/src/style/GlobalStyle.tsx
@@ -2,8 +2,9 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     * {
-        margin:0;
-        padding:0;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
     }
 `;
 
