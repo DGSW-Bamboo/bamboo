c89a9e95580ec183a993b97d93d2144826ce0290
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-03-25 13:25:14 +0900

    fix(web) styled-components 변경으로 인한 글로벌 스타일 수정

--- apps/web/src/style/GlobalStyle.tsx
+++ apps/web/src/style/GlobalStyle.tsx
@@ -1,18 +1,10 @@
-/** @jsx jsx */
-import { Global, css, jsx } from '@emotion/react';
+import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = () => {
-  return (
-    <Global
-      styles={css`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
-      `}
-    />
-  );
-};
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin:0;
+        padding:0;
+    }
+`;
 
 export default GlobalStyle;
